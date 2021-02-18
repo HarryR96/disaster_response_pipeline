125c5cac960f4cef8313d77892d4a9063b3c32a8
Harry Roper <harry.roper96@gmail.com>
2021-02-18 13:02:17 +0000

    bug fix in process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -60,7 +60,7 @@ def save_data(df, database_filepath):
     """
     
     engine = create_engine('sqlite:///{}'.format(database_filepath))
-    df.to_sql('messages', engine, index=False)
+    df.to_sql('messages', engine, index=False, if_exists='replace')
 
 def main():
     if len(sys.argv) == 4:
